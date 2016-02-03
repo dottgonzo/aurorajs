b8735c2ad580b908700105187be0c78890cfc465
Dario Caruso <darioyzf@gmail.com>
2016-02-03 17:37:47 +0100

    interface IAPI

--- index.ts
+++ index.ts
@@ -11,13 +11,50 @@ function prepare_address(addresses: IAddress[]) {
     return JSON.stringify(readdr);
 }
 
-interface IAPI{
-//todo
+interface Istring {
+    voltage: number;
+    current: number;
+    power: number;
 }
-interface IAddress{
-    uuid:string;
-    dev:string;
-    address:number;
+
+interface IAPI {
+
+    _id: string;
+    uid: string;
+    bootId: string;
+    bootTime: number;
+    active: boolean;
+    updatedAt: number;
+    date: string;
+    strings: Istring[];
+    grid: Istring;
+    DcAcCvrEff: number;
+    invTemp: number;
+    envTemp: number;
+    dailyEnergy: number;
+    weeklyEnergy: number;
+    last7DaysEnergy: number;
+    monthlyEnergy: number;
+    yearlyEnergy: number;
+    totalEnergy: number;
+    partialEnergy: number;
+    bulkV: number;
+    bulkMV: number;
+    bulkDC: number;
+    isoRes: number;
+    gridVDC: number;
+    gridAvgV: number;
+    gridDCHz: number;
+    peakMax: number;
+    peakDay: number;
+    pin1W: number;
+    pin2W: number;
+}
+
+interface IAddress {
+    uuid: string;
+    dev: string;
+    address: number;
 }
 class AJS {
     addresses: IAddress[];
@@ -30,7 +67,7 @@ class AJS {
         let addresses = prepare_address(this.addresses);
         let timezone = this.timezone;
         return new Promise<IAPI[]>(function(resolve, reject) {
-            exec(__dirname + "/aurora.sh -a \"" + addresses + "\" -t \"" + timezone + "\"").then(function(data:string) {
+            exec(__dirname + "/aurora.sh -a \"" + addresses + "\" -t \"" + timezone + "\"").then(function(data: string) {
                 resolve(JSON.parse(data));
             }).catch(function(err) {
                 reject(err);
