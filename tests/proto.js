b7eef96680d49c19d69870aa4367d5ea6661f684
dottgonzo <darioyzf@gmail.com>
2016-02-29 16:28:36 +0100

    up

--- tests/proto.js
+++ tests/proto.js
@@ -1,17 +1,13 @@
 var Aurora=require('../index.js'),
 
-verb=require("verbo");
+
 
 var aurorajs=new Aurora([{"uuid":"dkhlih","dev":"1-1.5","address":0}],"Europe/Rome");
 aurorajs.data().then(function(doc){
   if(doc){
-
-    verb(JSON.stringify(doc),"debug","Pure");
-
-
+    console.log(doc);
   } else{
-    verb("data problems","error","Aurorajs");
-
+    console.log("data problems","error","Aurorajs");
   }
 }).catch(function(err){
   verb(err,"error","raw")
